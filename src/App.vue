21703567053d38565f2091568e2fab285ad6dd0a
yuan-wifi <1372284129@qq.com>
2017-10-24 10:27:47 +0800

    ele头部完成

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <v-header :seller="seller"></v-header>
+    </div>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <!-- 使用 router-link 组件来导航. -->
+        <!-- 通过传入 `to` 属性指定链接. -->
+        <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import header from './components/header/header'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      seller: {},
+      goods: {},
+      ratings: {}
+    }
+  },
+  components: {
+    'v-header': header
+  },
+  created () {
+    // 获取商家信息
+    this.$axios.get('/api/seller').then((response) => {
+      this.seller = response.data.data
+      console.log(this.seller)
+    }, (error) => {
+      console.log(error)
+    })
+
+    // 调用商品信息
+    this.$axios.get('/api/goods').then((response) => {
+      this.goods = response.data
+      console.log(this.goods)
+    }, (error) => {
+      console.log(error)
+    })
+
+    // 获取评论信息
+    this.$axios.get('/api/ratings').then((response) => {
+      this.ratings = response.data
+      console.log(this.ratings)
+    }, (error) => {
+      console.log(error)
+    })
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import './common/stylus/mixin.styl'
+
+  #app
+    .tab
+      display: flex
+      width: 100%
+      height: 40px
+      line-height: 40px
+      // border-bottom: 1px solid rgba(7, 17,27, 0.1)
+      border-1px(rgba(7, 17,27, 0.1))
+      .tab-item
+        flex: 1
+        text-align: center
+        & > a
+          display: block
+          font-size: 14px
+          color: rgb(77, 85, 93)
+          &.active 
+            color: rgb(240, 20, 20)
+</style>
---
+++ src/components/goods/goods.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="goods">
+    dsgdfgfdgfdgfdgdfg
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'goods'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
